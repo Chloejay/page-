d040555af7342c39e2acb171fdfb2d1dfd3c2e37
Chloe Ji <chloejiy@gmail.com>
2017-12-18 03:55:56 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MainPage from './App';
+import './index.css'; 
+
+ReactDOM.render(
+    <MainPage   />,
+    document.getElementById('root'));  
+
